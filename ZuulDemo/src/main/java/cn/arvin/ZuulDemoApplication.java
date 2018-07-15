ce813fd148ac4af7f0e1f1c920082d579c82f50d
Arvin Xiao <1243276550@qq.com>
2018-07-15 17:23:32 +0800

    SpringCloud

---
+++ ZuulDemo/src/main/java/cn/arvin/ZuulDemoApplication.java
@@ -0,0 +1,19 @@
+package cn.arvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+@EnableZuulProxy // 开启Zuul的网关功能
+@EnableDiscoveryClient
+public class ZuulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulDemoApplication.class, args);
+	}
+}
